db04cb52ce3a985ab19f892a3262361cc7dac604
edupoly <info.edupoly@gmail.com>
2024-04-24 19:08:09 +0530

    sdds

--- src/Testimonial.jsx
+++ src/Testimonial.jsx
@@ -23,23 +23,31 @@ export default function Testimonial() {
         })
     }
     return (
-        <div>
+        <div className='d-flex flex-column align-items-center'>
             <h1>Testimonial</h1>
-            <ul class="d-flex justify-content-center align-items-center">
-                
-                    <i class="bi bi-arrow-left-square-fill" onClick={()=>{prev()}}></i>
-                    {
-                        <li className='w-25'>
-                            <img className='w-100 thumbnail'  src={reviews[index].image} alt="" />
-                            <h3>{reviews[index].name}</h3>
-                            <i>{reviews[index].job}</i>
-                            <p>{reviews[index].text}</p>
-                        </li>
-                    }
-                    <i class="bi bi-arrow-right-square-fill" onClick={()=>{next()}}></i>
-                
-                
-            </ul>
+            <div>
+                <ul class="d-flex justify-content-center align-items-center">
+                    
+                        <i class="bi bi-arrow-left-square-fill" onClick={()=>{prev()}}></i>
+                        {
+                            <li className='w-25'>
+                                <img className='w-100 thumbnail'  src={reviews[index].image} alt="" />
+                                <h3>{reviews[index].name}</h3>
+                                <i>{reviews[index].job}</i>
+                                <p>{reviews[index].text.slice(0,150)}</p>
+                            </li>
+                        }
+                        <i class="bi bi-arrow-right-square-fill" onClick={()=>{next()}}></i>
+                </ul>
+            </div>
+            <div className='d-flex flex-row align-items-center'>
+                {
+                    reviews.map((r,i)=>{
+                        return <i className={i===index?"bi bi-dash display-1":"bi bi-dot display-5"}
+                         onClick={()=>{setIndex(i)}}></i>
+                    })
+                }
+            </div>
         </div>
     )
 }
