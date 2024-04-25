4746e02f49d6e263ecc728bfb44247c29ab12fe6
edupoly <info.edupoly@gmail.com>
2024-04-25 21:11:25 +0530

    dfsd

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState,useRef } from "react";
 
 
 function App() {
-  var [min,setMin] = useState(5)
+  var [min,setMin] = useState(0)
   var [sec,setSec] = useState(0)
   var [startFlag,setStartFlag]=useState(false)
   var timerRef = useRef();
@@ -11,17 +11,17 @@ function App() {
   function startTimer(){
     timerRef.current=setInterval(()=>{
       setSec((oldsec)=>{
-        if(oldsec===0){
+        if(oldsec===59){
           setMin(oldmin=>{
-            return oldmin-1
+            return oldmin+1
           })
-          return 59
+          return 0
         }
         else{
-          return oldsec-1
+          return oldsec+1
         }
       })
-    },100)
+    },300)
   }
   useEffect(()=>{
     if(startFlag){
@@ -42,14 +42,14 @@ function App() {
   function resetTimer(){
     clearInterval(timerRef.current)
     setStartFlag(false)
-    setMin(5);
+    setMin(0);
     setSec(0)
   }
   return (
-    <div>
+    <div className="w-50 text-center mx-auto m-5 border border-2 p-3">
       <h3>Timer</h3>
       <h1>{min}:{sec}</h1>
-      <button onClick={()=>{startToggle()}}>{startFlag?'pause':'start'}</button>
+      <button onClick={()=>{startToggle()}}>{startFlag?'Pause':'Start'}</button>
       <button onClick={()=>{resetTimer()}}>Reset</button>
     </div>
   );
