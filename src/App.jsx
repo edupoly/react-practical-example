acbd01367f758b9b595f778d775f6a851fb7949c
edupoly <info.edupoly@gmail.com>
2024-04-26 07:33:00 +0530

    sdfsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 import { useState } from "react";
-
-
 function App() {
-  var [billamount,setbillamount]=useState(0)
-  var [tip,settip]=useState(0)
-  var [total,settotal]=useState(0)
-  function totalBill(){
-    console.log("HI")
-    settotal(()=>{
-      return billamount+(billamount*tip/100)
-    })
+  var [dob,setdob]=useState()
+  var [age,setage]=useState(0)
+  function totalAge(){
+    var ctime = (new Date()).getTime();
+    var btime = (new Date(dob)).getTime();
+    var atime = ctime-btime;
+    setage(()=>atime/(1000*60*60*24*365))
   }
   return (
     <div className="w-50 mx-auto m-5 border border-2 p-3 bg-white rounded-4">
       <h1 className=" text-center">Tip Calculator</h1>
-      <h3>Whats your Bill!??</h3>
+      <h3>Whats your Date Of Birth?</h3>
       <div class="form-floating mb-3">
-        <input type="text" onChange={(e)=>{setbillamount(+e.target.value)}} class="form-control" id="billamount" placeholder="Rs.1000"/>
-        <label for="billamount">Bill Amount</label>
-      </div>
-      <div class="form-floating mb-3">
-        <input type="text" onChange={(e)=>{settip(+e.target.value)}} class="form-control" id="tippercentage" placeholder="10%"/>
-        <label for="tippercentage">Tip Percentage</label>
+        <input type="date" onChange={(e)=>{setdob(e.target.value)}} class="form-control" id="dob" placeholder="Rs.1000"/>
+        <label for="dob">Date of Birth</label>
       </div>
       <div class="d-grid gap-2">
-        <button class="btn btn-primary" type="button" onClick={()=>{totalBill()}}>CALCULATE TOTAL BILL</button>
+        <button class="btn btn-primary" type="button" onClick={()=>{totalAge()}}>CALCULATE My AGE</button>
       </div>
-      <div className="my-4"><span className="blockquote">Total: </span><b class="h2">Rs.{total}</b></div>
+      <div className="my-4"><span className="blockquote">Age: </span><b class="h2">{age}</b></div>
     </div>
   );
 }
